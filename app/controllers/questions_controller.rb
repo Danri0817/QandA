8541607838a4e9ec57a7b7ca15d77b761dd5e6c2
EC2 Default User <ec2-user@ip-172-31-33-34.ap-northeast-1.compute.internal>
2019-06-20 10:05:57 +0000

    QandA

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,8 +7,36 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
+  end
+  
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: '成功！'
+    else
+      render :new
+      flash[:alert] = '失敗！'
+    end
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
+  
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to root_path, notice: '成功！'
+    else
+      flash[:alert] = '失敗'
+      render :edit
+    end
+  end
+  
+  private
+  
+    def question_params
+      params.require(:question).permit(:name, :title, :content)
+    end
 end
